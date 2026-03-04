d6e9e779549c0998c68564913d70014efb0a7cd1
Joffrey <152586527+Joffrey8489@users.noreply.github.com>
2026-03-04 21:32:24 +0200

    final ataches

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Task } from "./components/task/task";
+import { Footer } from "./components/footer/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Task],
+  imports: [RouterOutlet, Header, Task, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
